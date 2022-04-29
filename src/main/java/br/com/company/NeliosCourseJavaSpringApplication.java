b1ea1a79e9e599e710ee2a01f76d50066a1e8461
Andrew Phillipe <andrewphillipe@hotmail.com>
2022-04-29 15:36:32 -0300

    Project Created!

---
+++ src/main/java/br/com/company/NeliosCourseJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeliosCourseJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeliosCourseJavaSpringApplication.class, args);
+	}
+
+}
