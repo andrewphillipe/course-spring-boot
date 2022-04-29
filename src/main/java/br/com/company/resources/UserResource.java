01f6115be6897537960aa115a75f17c236bf42cb
Andrew Phillipe <andrewphillipe@hotmail.com>
2022-04-29 15:56:31 -0300

    Create User entity and UserResource

---
+++ src/main/java/br/com/company/resources/UserResource.java
@@ -0,0 +1,20 @@
+package br.com.company.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.company.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L, "" , "", "", "");
+		return ResponseEntity.ok().body(user);
+	} 
+	
+}
